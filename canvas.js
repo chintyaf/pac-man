09e048d50a9865302ad5489f08d8e79886430222
Elika N <153571556+blissels@users.noreply.github.com>
2025-10-28 22:11:07 +0700

    Pac-Man
    1. Udah bisa gerak sambil buka mulut
    2. Bisa baca tembok (tapi masih ada yang miss kalo nabrak garis)
    3. Gerak pake panah

--- canvas.js
+++ canvas.js
@@ -4,13 +4,38 @@ cnv = document.querySelector("#canvas");
 ctx = cnv.getContext("2d");
 imageDataA = ctx.getImageData(0, 0, cnv.width, cnv.height);
 
-generateMaze(imageDataA, ctx, cnv, 35);
+var mazeData = null;
 
-ctx.putImageData(imageDataA, 0, 0);
+// buat generate maze dan jalanin Pac-Man
+function startGame() {
+    try {
+        mazeData = generateMaze(imageDataA, ctx, cnv, 35);
+    } catch (e) {
+        console.error("Gagal menjalankan generateMaze.");
+        console.error(e);
+    }
+    initializePacman(ctx, cnv, mazeData); 
+    if (mazeData && !mazeData.animating) { 
+        ctx.putImageData(imageDataA, 0, 0);
+    }
+    gameLoop();
+}
+
+// Loop game, buat gambar ulang labirin dan update Pac-Man
+function gameLoop() {
+    ctx.putImageData(imageDataA, 0, 0); 
+
+    updatePosition(); 
+    drawPacman(); 
+    
+    requestAnimationFrame(gameLoop);
+}
 
 cnv.addEventListener("click", function (event) {
-    var rect = cnv.getBoundingClientRect();
-    var x = event.clientX - rect.left;
-    var y = event.clientY - rect.top;
+    const rect = cnv.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
     console.log("x: " + x + " y: " + y);
 });
+
+startGame();
