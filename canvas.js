3239872cffe791166dc84d8331ff5e75728fabfb
Elika N <153571556+blissels@users.noreply.github.com>
2025-10-28 22:07:50 +0700

    Pindah branch

--- canvas.js
+++ canvas.js
@@ -4,38 +4,13 @@ cnv = document.querySelector("#canvas");
 ctx = cnv.getContext("2d");
 imageDataA = ctx.getImageData(0, 0, cnv.width, cnv.height);
 
-var mazeData = null;
+generateMaze(imageDataA, ctx, cnv, 35);
 
-// buat generate maze dan jalanin Pac-Man
-function startGame() {
-    try {
-        mazeData = generateMaze(imageDataA, ctx, cnv, 35);
-    } catch (e) {
-        console.error("Gagal menjalankan generateMaze.");
-        console.error(e);
-    }
-    initializePacman(ctx, cnv, mazeData); 
-    if (mazeData && !mazeData.animating) { 
-        ctx.putImageData(imageDataA, 0, 0);
-    }
-    gameLoop();
-}
-
-// Loop game, buat gambar ulang labirin dan update Pac-Man
-function gameLoop() {
-    ctx.putImageData(imageDataA, 0, 0); 
-
-    updatePosition(); 
-    drawPacman(); 
-    
-    requestAnimationFrame(gameLoop);
-}
+ctx.putImageData(imageDataA, 0, 0);
 
 cnv.addEventListener("click", function (event) {
-    const rect = cnv.getBoundingClientRect();
-    const x = event.clientX - rect.left;
-    const y = event.clientY - rect.top;
+    var rect = cnv.getBoundingClientRect();
+    var x = event.clientX - rect.left;
+    var y = event.clientY - rect.top;
     console.log("x: " + x + " y: " + y);
 });
-
-startGame();
