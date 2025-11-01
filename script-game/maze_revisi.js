c26acdcdaa25279d71f09443c0fce17aea2c0811
chintyaf <chintya.elysia@gmail.com>
2025-11-01 11:52:13 +0700

    update maze

--- script-game/maze_revisi.js
+++ script-game/maze_revisi.js
@@ -2,6 +2,69 @@
 window.grid = [];
 
 function generateMaze(imageDataA, ctx, cnv, cell_width) {
+    let delay = document.getElementById("speed").value;
+
+    class Cell {
+        constructor(i, j) {
+            this.i = i;
+            this.j = j;
+            this.walls = [true, true, true, true]; // top, right, bottom, left
+            this.highlight = 0; // 0 = normal, >0 = highlight intensity
+            this.sudahTerhubung = false; // apakah sudah terhubung dengan kotak lain?
+            this.sedangDicek = false; // sedang diperiksa sekarang?
+        }
+
+        show() {
+            const x = this.i * w;
+            const y = this.j * w;
+
+            let r, g, b;
+
+            if (this.sedangDicek) {
+                // kuning
+                r = 255;
+                g = 255;
+                b = 100;
+            } else if (this.highlight > 0) {
+                // hijau -- berhasil dihubungkan
+                r = 150;
+                g = 255;
+                b = 150;
+            } else if (this.sudahTerhubung) {
+                // biru muda -- sudah jadi bagian dari labirin
+                r = 150;
+                g = 255;
+                b = 250;
+            } else {
+                // putih -- belum dikunjungi
+                r = 255;
+                g = 255;
+                b = 255;
+            }
+
+            // Draw pixels
+            for (let px = 1; px < w - 1; px++) {
+                for (let py = 1; py < w - 1; py++) {
+                    gbr_titik(imageDataA, x + px, y + py, r, g, b);
+                }
+            }
+
+            // Walls line
+            if (this.walls[0]) {
+                dda_line(imageDataA, x, y, x + w, y, 0, 0, 0);
+            }
+            if (this.walls[1]) {
+                dda_line(imageDataA, x + w, y, x + w, y + w, 0, 0, 0);
+            }
+            if (this.walls[2]) {
+                dda_line(imageDataA, x + w, y + w, x, y + w, 0, 0, 0);
+            }
+            if (this.walls[3]) {
+                dda_line(imageDataA, x, y + w, x, y, 0, 0, 0);
+            }
+        }
+    }
+
     class DisjointSet {
         constructor(n) {
             this.parent = Array.from({ length: n }, (_, i) => i);
@@ -12,63 +75,29 @@ function generateMaze(imageDataA, ctx, cnv, cell_width) {
                 this.parent[x] = this.find(this.parent[x]);
             return this.parent[x];
         }
+
         union(x, y) {
             let rx = this.find(x);
             let ry = this.find(y);
-            if (rx === ry) return false;
-            if (this.rank[rx] < this.rank[ry]) this.parent[rx] = ry;
-            else if (this.rank[rx] > this.rank[ry]) this.parent[ry] = rx;
-            else {
+
+            // sudah di dalma grup yang sama
+            if (rx === ry) {
+                return false;
+            }
+
+            if (this.rank[rx] < this.rank[ry]) {
+                this.parent[rx] = ry;
+            } else if (this.rank[rx] > this.rank[ry]) {
+                this.parent[ry] = rx;
+            } else {
                 this.parent[ry] = rx;
                 this.rank[rx]++;
             }
+
             return true;
         }
     }
 
-    class Cell {
-        constructor(i, j) {
-            this.i = i;
-            this.j = j;
-            this.walls = [true, true, true, true]; // top, right, bottom, left
-            this.highlight = 0; // 0 = normal, >0 = highlight intensity
-        }
-
-        show() {
-            const x = this.i * w;
-            const y = this.j * w;
-
-            // Background
-            let brightness =
-                this.highlight > 0 ? 180 + this.highlight * 60 : 255;
-            for (let px = 1; px < w - 1; px++) {
-                for (let py = 1; py < w - 1; py++) {
-                    gbr_titik(
-                        imageDataA,
-                        x + px,
-                        y + py,
-                        brightness,
-                        255,
-                        brightness
-                    );
-                }
-            }
-
-            // Walls line
-            if (this.walls[0]) dda_line(imageDataA, x, y, x + w, y, 0, 0, 0);
-            if (this.walls[1])
-                dda_line(imageDataA, x + w, y, x + w, y + w, 0, 0, 0);
-            if (this.walls[2])
-                dda_line(imageDataA, x + w, y + w, x, y + w, 0, 0, 0);
-            if (this.walls[3]) dda_line(imageDataA, x, y + w, x, y, 0, 0, 0);
-        }
-    }
-
-    const w = cell_width;
-    const cols = Math.floor(cnv.width / w);
-    const rows = Math.floor(cnv.height / w);
-    
-    // Clear dan inisialisasi grid global
     window.grid = [];
     const grid = window.grid;
 
@@ -76,6 +105,12 @@ function generateMaze(imageDataA, ctx, cnv, cell_width) {
         if (i < 0 || j < 0 || i >= cols || j >= rows) return -1;
         return i + j * cols;
     }
+
+    const w = cell_width;
+    const cols = Math.floor(cnv.width / w);
+    const rows = Math.floor(cnv.height / w);
+    // const cols = 10;
+    // const rows = 5;
 
     // inisiasi grid cell
     for (let j = 0; j < rows; j++) {
@@ -89,8 +124,12 @@ function generateMaze(imageDataA, ctx, cnv, cell_width) {
     for (let i = 0; i < cols; i++) {
         for (let j = 0; j < rows; j++) {
             let a = index(i, j);
-            if (i < cols - 1) walls.push([a, index(i + 1, j), "V"]);
-            if (j < rows - 1) walls.push([a, index(i, j + 1), "H"]);
+            if (i < cols - 1) {
+                walls.push([a, index(i + 1, j), "V"]);
+            }
+            if (j < rows - 1) {
+                walls.push([a, index(i, j + 1), "H"]);
+            }
         }
     }
     walls.sort(() => Math.random() - 0.5);
@@ -116,8 +155,20 @@ function generateMaze(imageDataA, ctx, cnv, cell_width) {
 
     function drawGrid() {
         clearCanvas(255, 255, 255);
+
+        // Kurangi highlight secara bertahap (efek fade)
         for (let c of grid) {
-            if (c.highlight > 0) c.highlight -= 0.05;
+            if (c.highlight > 0) {
+                c.highlight -= 0.1;
+                if (c.highlight <= 0) {
+                    c.highlight = 0;
+                    c.sudahTerhubung = true;
+                }
+            }
+        }
+
+        // Gambar semua kotak
+        for (let c of grid) {
             c.show();
         }
         ctx.putImageData(imageDataA, 0, 0);
@@ -125,9 +176,13 @@ function generateMaze(imageDataA, ctx, cnv, cell_width) {
 
     const ds = new DisjointSet(grid.length);
     let wallIndex = 0;
-    let batchSize = 5;
-    
+    let batchSize = 1;
+
     function animateKruskal() {
+        for (let c of grid) {
+            c.sedangDicek = false;
+        }
+
         for (
             let n = 0;
             n < batchSize && wallIndex < walls.length;
@@ -137,23 +192,48 @@ function generateMaze(imageDataA, ctx, cnv, cell_width) {
             const a = grid[aIdx];
             const b = grid[bIdx];
 
+            a.sedangDicek = true;
+            b.sedangDicek = true;
+
+            // Cari grup masing-masing
+            const grupA = ds.find(aIdx);
+            const grupB = ds.find(bIdx);
+            // console.log(`Kotak [${a.i},${a.j}] ada di grup #${grupA}`);
+            // console.log(`Kotak [${b.i},${b.j}] ada di grup #${grupB}`);
+
+            // cek apakah kedua kotak ini sudah terhubung?
+            // klo belum
             if (ds.union(aIdx, bIdx)) {
                 removeWall(a, b);
                 a.highlight = 1;
                 b.highlight = 1;
+                a.sedangDicek = false;
+                b.sedangDicek = false;
             }
         }
 
         drawGrid();
 
         if (wallIndex < walls.length) {
-            requestAnimationFrame(animateKruskal);
+            setTimeout(() => {
+                requestAnimationFrame(animateKruskal);
+            }, delay);
         } else {
+            for (let c of grid) {
+                c.sedangDicek = false;
+            }
             console.log("Maze complete! Grid available:", window.grid.length);
             drawGrid();
         }
     }
 
+    // function animateDraw() {
+    //     drawGrid();
+    //     requestAnimationFrame(animateDraw);
+    // }
+
     drawGrid();
-    animateKruskal();
+    setTimeout(() => {
+        animateKruskal();
+    }, delay); // Jeda 1 detik sebelum mulai
 }
